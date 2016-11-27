9853bc79ac765736d8df0c0d5493d8ae8aadc8cc
Michal, Polcik <michal.polcik@teamaol.com>
2016-11-28 00:38:30 +0100

    remove null from default value

--- src/main/groovy/jobBuilder/Utils/Steps.groovy
+++ src/main/groovy/jobBuilder/Utils/Steps.groovy
@@ -2,12 +2,12 @@ package jobBuilder.Utils
 
 class Steps {
 
-    static void proxiedGradle(context, String gradleTasks,String gradleSwitches = null) {
+    static void proxiedGradle(context, String gradleTasks,String gradleSwitches = "") {
         context.with {
             gradle {
                 useWrapper true
                 tasks gradleTasks
-                switches gradleSwitches?.stripIndent()?.trim()
+                switches gradleSwitches.stripIndent().trim()
             }
         }
     }
